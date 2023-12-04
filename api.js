e86a9dfdc9e197d312ada59ab8cb2cc1e5a11ff5
Douglas Santos <foxgoldseller@gmail.com>
2023-12-04 15:44:03 -0300

    searchbox done

--- api.js
+++ api.js
@@ -1,4 +1,3 @@
-const name = prompt("Digite o nome:");
 const baseUrl = `https://rickandmortyapi.com/api/character/?name=${name}`;
 
 axios
@@ -33,3 +32,42 @@ axios
   .catch((error) => {
     console.error("Error:", error);
   });
+
+function searchCharacters() {
+  const searchTerm = document.getElementById("searchInput").value;
+  const newBaseUrl = `https://rickandmortyapi.com/api/character/?name=${searchTerm}`;
+
+  axios
+    .get(newBaseUrl)
+    .then((response) => {
+      const personagens = response.data.results;
+      const listaElement = document.getElementById("lista");
+      listaElement.innerHTML = "";
+
+      personagens.forEach((personagem) => {
+        const liElement = document.createElement("li");
+        liElement.classList.add("character-item");
+
+        const imgElement = document.createElement("img");
+        imgElement.src = personagem.image;
+        liElement.appendChild(imgElement);
+
+        const infoElement = document.createElement("div");
+
+        const camposExibir = ["id", "name", "status", "species", "gender"];
+
+        camposExibir.forEach((campo) => {
+          const spanElement = document.createElement("span");
+          spanElement.textContent = `${campo}: ${personagem[campo]}`;
+          infoElement.appendChild(spanElement);
+        });
+
+        liElement.appendChild(infoElement);
+
+        listaElement.appendChild(liElement);
+      });
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
